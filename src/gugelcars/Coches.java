79a95574f4d496807d354e9a75f44860f91601ce
Adrian <adry_inter@hotmail.com>
2018-11-26 22:37:34 +0100

    Esqueleto
    Creación del esqueleto

---
+++ src/gugelcars/Coches.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gugelcars;
+
+import DBA.SuperAgent;
+import es.upv.dsic.gti_ia.core.AgentID;
+
+/**
+ *
+ * @author Adrian Martin Jaimez
+ */
+public class Coches extends SuperAgent {
+    
+    public Coches(AgentID aid) throws Exception {
+        super(aid);
+    }
+    
+}
---
+++ src/gugelcars/Coordinador.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gugelcars;
+
+import DBA.SuperAgent;
+import es.upv.dsic.gti_ia.core.AgentID;
+
+/**
+ *
+ * @author Adrian Martin Jaimez
+ */
+public class Coordinador extends SuperAgent {
+    
+    public Coordinador(AgentID aid) throws Exception {
+        super(aid);
+    }
+    
+}
