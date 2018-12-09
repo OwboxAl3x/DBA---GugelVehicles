9d246490ea79176d63dfa1ff4f91a04a02823f95
fernandorh97 <43602504+fernandorh97@users.noreply.github.com>
2018-12-09 21:35:35 +0100

    Algoritmo objetivo y Crear escáner según cuadrante

--- src/gugelcars/Coches.java
+++ src/gugelcars/Coches.java
@@ -10,6 +10,9 @@ import com.eclipsesource.json.Json;
 import com.eclipsesource.json.JsonObject;
 import es.upv.dsic.gti_ia.core.ACLMessage;
 import es.upv.dsic.gti_ia.core.AgentID;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  *
@@ -32,6 +35,12 @@ public class Coches extends SuperAgent {
     private int cuadrante;
     private boolean puedoVolar;
     private int prioridad;
+    
+    private double bateria = 0.0;
+    private int x;
+    private int y;
+    private ArrayList<ArrayList<Integer>> mapaPasos = new ArrayList<>();
+    private ArrayList<ArrayList<Float>> mapaEscaner = new ArrayList<>();
     
     public Coches(AgentID aid, String nombreCoordinador, String nombreCoche1, String nombreCoche2, String nombreCoche3, String nombreCoche4) throws Exception {
         super(aid);
@@ -299,6 +308,280 @@ public class Coches extends SuperAgent {
             //no me muevo
         }
     }
+    
+    /**
+     * Obtiene el valor del escaner de una casilla
+     * @author Fernando Ruiz Hernández
+     * @author Adrian Martin 
+     */
+    
+    public float getValorEscaner(int x, int y, int celda) {
+        float valor = Float.MAX_VALUE;
+        switch (celda){
+            case 6:
+                valor = this.mapaEscaner.get(y-1).get(x-1);
+                break;
+            case 7:
+                valor = this.mapaEscaner.get(y-1).get(x);
+                break;
+            case 8:
+                valor = this.mapaEscaner.get(y-1).get(x+1);
+                break;
+            case 11:
+                valor = this.mapaEscaner.get(y).get(x-1);
+                break;
+            case 13:
+                valor = this.mapaEscaner.get(y).get(x+1);
+                break;
+            case 16:
+                valor = this.mapaEscaner.get(y+1).get(x-1);
+                break;
+            case 17:
+                valor = this.mapaEscaner.get(y+1).get(x);
+                break;
+            case 18:
+                valor = this.mapaEscaner.get(y+1).get(x+1);
+                break;
+        }
+        return valor;
+    }
+    
+    /**
+     * Obtiene el valor de pasos de una casilla
+     * @author Fernando Ruiz Hernández
+     * @author Adrian Martin 
+     */
+    
+    public int getValorPasos(int x, int y, int celda) {
+        int valor = Integer.MAX_VALUE;
+        switch (celda){
+            case 6:
+                valor = this.mapaPasos.get(y-1).get(x-1);
+                break;
+            case 7:
+                valor = this.mapaPasos.get(y-1).get(x);
+                break;
+            case 8:
+                valor = this.mapaPasos.get(y-1).get(x+1);
+                break;
+            case 11:
+                valor = this.mapaPasos.get(y).get(x-1);
+                break;
+            case 13:
+                valor = this.mapaPasos.get(y).get(x+1);
+                break;
+            case 16:
+                valor = this.mapaPasos.get(y+1).get(x-1);
+                break;
+            case 17:
+                valor = this.mapaPasos.get(y+1).get(x);
+                break;
+            case 18:
+                valor = this.mapaPasos.get(y+1).get(x+1);
+                break;
+        }
+        return valor;
+    }
+    
+    /**
+     * Actualiza el mapa de los pasos. Cada casilla contiene el número de veces
+     * que se ha pasado por ella.
+     * @author Fernando Ruiz Hernández
+     * @param percepcion Objeto JSON con la percepción recibida
+     */
+    public void actualizarMapaPasos(JsonObject percepcion) {
+        // Coordenadas de posición
+        x = percepcion.get("gps").asObject().get("x").asInt();
+        y = percepcion.get("gps").asObject().get("y").asInt();
+        
+        // Ajustar tamaño del mapa si es necesario
+        int sizeNuevo = x + 3;
+        if (sizeNuevo < y + 3)
+            sizeNuevo = y + 3;
+        if (sizeNuevo > mapaPasos.size())
+            this.extenderMapaPasos(sizeNuevo);
+        
+        // Actualizar casillas
+        int casilla_valor = mapaPasos.get(y).get(x);
+        mapaPasos.get(y).set(x, casilla_valor+1);
+    }
+    
+    
+    /**
+     * Extiende el tamaño del mapa de pasos. Se rellena con el valor 0.
+     * 
+     * @author Fernando Ruiz Hernández
+     * @param sizeNuevo Tamaño nuevo
+     */
+    public void extenderMapaPasos(int sizeNuevo) {
+        int size = mapaPasos.size();
+        
+        // Extender filas existentes
+        for (int i=0; i<size; i++) {
+            for (int j=size; j<sizeNuevo; j++) {
+                mapaPasos.get(i).add(0);
+            }
+        }
+
+        // Añadir nuevas filas
+        ArrayList<Integer> fila;
+        for (int i=size; i<sizeNuevo; i++) {
+            fila = new ArrayList<>();
+            for (int j=0; j<sizeNuevo; j++) {
+                fila.add(0);
+            }
+            mapaPasos.add(fila);
+        }
+    }
+    
+    /**
+     * Construye el mapaEscaner para teniendo como objetivo un cuadrante
+     * @author Fernando Ruiz Hernández
+     */
+    public void construirEscanerCuadrante(int size) {
+        
+        int x_objetivo = (size * (1 + (cuadrante % 2)))/4;
+        int y_objetivo = (size * (1 + (cuadrante / 2)))/4;;
+        float distancia;
+        
+        // Añadir nuevas filas
+        ArrayList<Float> fila;
+        for (int i=0; i<size; i++) {
+            fila = new ArrayList<>();
+            for (int j=0; j<size; j++) {
+                distancia = (float) Math.sqrt(
+                    (x_objetivo - j) *  (x_objetivo - j) + 
+                    (y_objetivo - i) *  (y_objetivo - i)
+                );
+                fila.add(distancia);
+            }
+            mapaEscaner.add(fila);
+        }
+    }
+    
+    /**
+     * Se mueve hacia el objetivo.
+     * 
+     * @author Fernando Ruiz Hernández
+     * 
+     */
+    public void irObjetivo(JsonObject percepcionJson){
+        boolean salir = false;
+        
+        while (!salir){
+            
+            // *** Comprobar si tiene que hacer refuel
+            if(bateria <= 1.0){
+                //refuel();
+            }else if (percepcionJson.get("radar").asArray().get(12).asInt() != 2){
+                // Coordenadas de posición
+                x = percepcionJson.get("gps").asObject().get("x").asInt();
+                y = percepcionJson.get("gps").asObject().get("y").asInt();
+                
+                // Algoritmo de cálculo de movimiento
+                int minimo = Integer.MAX_VALUE;
+                
+                TreeMap<Float,String> casillas = new TreeMap<Float,String>();
+                
+                // Calculamos mínimo
+                if (percepcionJson.get("radar").asArray().get(6).asInt() != 1){
+                    if(minimo >= this.getValorPasos(x, y,6)){
+                        minimo = this.getValorPasos(x, y,6);
+                    }
+                }
+                if (percepcionJson.get("radar").asArray().get(7).asInt() != 1){
+                    if(minimo >= this.getValorPasos(x, y, 7)){
+                        minimo = this.getValorPasos(x, y, 7);
+                    }
+                }
+                if (percepcionJson.get("radar").asArray().get(8).asInt() != 1){
+                    if(minimo >= this.getValorPasos(x, y, 8)){
+                        minimo = this.getValorPasos(x, y, 8);
+                    }
+                }
+                if (percepcionJson.get("radar").asArray().get(11).asInt() != 1){
+                    if(minimo >= this.getValorPasos(x, y, 11)){
+                        minimo = this.getValorPasos(x, y, 11);
+                    }
+                }
+                if (percepcionJson.get("radar").asArray().get(13).asInt() != 1){
+                    if(minimo >= this.getValorPasos(x, y, 13)){
+                        minimo = this.getValorPasos(x, y, 13); 
+                    }
+                }
+                if (percepcionJson.get("radar").asArray().get(16).asInt() != 1){
+                    if(minimo >= this.getValorPasos(x, y, 16)){
+                        minimo = this.getValorPasos(x, y, 16);
+                    }
+                }
+                if (percepcionJson.get("radar").asArray().get(17).asInt() != 1){
+                    if(minimo >= this.getValorPasos(x, y, 17)){
+                        minimo = this.getValorPasos(x, y, 17);
+                    }
+                }
+                if (percepcionJson.get("radar").asArray().get(18).asInt() != 1){
+                    if(minimo >= this.getValorPasos(x, y, 18)){
+                        minimo = this.getValorPasos(x, y, 18);
+                    }
+                }
+                
+                // Añadir casillas
+                if (percepcionJson.get("radar").asArray().get(6).asInt() != 1){
+                    if(minimo >= this.getValorPasos(x, y,6)){
+                        casillas.put(getValorEscaner(x, y, 6), "NW");
+                    }
+                }
+                if (percepcionJson.get("radar").asArray().get(7).asInt() != 1){
+                    if(minimo >= this.getValorPasos(x, y, 7)){
+                        casillas.put(getValorEscaner(x, y, 7), "N");
+                    }
+                }
+                if (percepcionJson.get("radar").asArray().get(8).asInt() != 1){
+                    if(minimo >= this.getValorPasos(x, y, 8)){
+                        casillas.put(getValorEscaner(x, y, 8), "NE");
+                    }
+                }
+                if (percepcionJson.get("radar").asArray().get(11).asInt() != 1){
+                    if(minimo >= this.getValorPasos(x, y, 11)){
+                        casillas.put(getValorEscaner(x, y, 11), "W");
+                    }
+                }
+                if (percepcionJson.get("radar").asArray().get(13).asInt() != 1){
+                    if(minimo >= this.getValorPasos(x, y, 13)){
+                        casillas.put(getValorEscaner(x, y, 13), "E"); 
+                    }
+                }
+                if (percepcionJson.get("radar").asArray().get(16).asInt() != 1){
+                    if(minimo >= this.getValorPasos(x, y, 16)){
+                        casillas.put(getValorEscaner(x, y, 16), "SW");
+                    }
+                }
+                if (percepcionJson.get("radar").asArray().get(17).asInt() != 1){
+                    if(minimo >= this.getValorPasos(x, y, 17)){
+                        casillas.put(getValorEscaner(x, y, 17), "S");
+                    }
+                }
+                if (percepcionJson.get("radar").asArray().get(18).asInt() != 1){
+                    if(minimo >= this.getValorPasos(x, y, 18)){
+                        casillas.put(getValorEscaner(x, y, 18), "SE");
+                    }
+                }
+                
+                Map.Entry<Float,String> casillaResultado = casillas.firstEntry();
+                
+                
+                //this.moverse("move"+casillaResultado.getValue());
+                
+                bateria--;
+            } else {
+                // logout
+                System.out.println("Hemos llegado al objetivo.");
+                salir = true;
+                //this.logout();
+            }
+        }
+    }
+    
     /**
     *
     * @author Manuel Ros Rodríguez
